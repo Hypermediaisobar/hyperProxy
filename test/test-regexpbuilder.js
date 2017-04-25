c1aba4eb1286ce8d1b7d720690c529554be6bd7f
ahwayakchih <ahwayakchih@gmail.com>
2017-04-25 18:22:59 +0200

    Added another case to test of regexpbuilder

--- test/test-regexpbuilder.js
+++ test/test-regexpbuilder.js
@@ -88,6 +88,17 @@ describe('RegExpBuilder', function(){
 			],
 			expected: 'foo(?:★)?',
 			antiString: 'zzzzzzz'
+		},
+		{
+			strings: [
+				'foobar',
+				'foobaz',
+				'foozap',
+				'fooza',
+				'farza'
+			],
+			expected: 'f(?:arza|oo(?:ba[rz]|zap?))',
+			antiString: 'zzzzzzz'
 		}
 	];
 
